ab1387d9f130baf3755811725bd1e97ca60d753a
Retrooff <abdoulkone549@gmail.com>
2025-06-06 07:00:09 +0000

    bot.py

--- bot.py
+++ bot.py
@@ -12,8 +12,11 @@ ADMIN_ID = 8121512840
 
 DB_FILE = "users.json"
 
-def load_data(): if not os.path.exists(DB_FILE): return {} with open(DB_FILE, 'r') as f: return json.load(f)
-
+def load_data():
+    if not os.path.exists(DB_FILE):
+        return {}
+    with open(DB_FILE, 'r') as f:
+        return json.load(f)
 def save_data(data): with open(DB_FILE, 'w') as f: json.dump(data, f, indent=2)
 
 data = load_data() bot = telebot.TeleBot(TOKEN, parse_mode='HTML')
